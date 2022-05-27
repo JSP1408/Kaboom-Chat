21b904efc7cfc19f2ba35816b244d17225fe2285
VosmoslaGod <the.trickster215@gmail.com>
2022-05-27 19:52:40 -0400

    update

--- app.js
+++ app.js
@@ -109,7 +109,12 @@ function client() {
 
   bot.on("end", (reason) => {
     console.log(`Client exited with: ${reason}, attempting reconnection`)
-    reload("./app.js");
+    bot._client.end()
+    const bot = new Bot({
+        host: settings.IP,
+        port: settings.Port,
+        username: settings.username,
+      });
   });
 }
 
